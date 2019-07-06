a7d6369c4ff21d99c1b469e937bf114c694d606c
martaprosniak <martaprosniak@gmail.com>
2019-07-07 00:35:40 +0200

    snake can be controlled

--- snake.js
+++ snake.js
@@ -1,63 +1,88 @@
-const cvs = document.getElementById("snake")
+const cvs = document.getElementById("snake");
 const ctx = cvs.getContext("2d");
 
 // create the unit
-const box = 32
+const box = 32;
 
 // load images
-const ground = new Image()
-ground.src = "img/ground.png"
+const ground = new Image();
+ground.src = "img/ground.png";
 
 const foodImg = new Image();
-foodImg.src = "img/food.png"
+foodImg.src = "img/food.png";
 
 // create the snake
-let snake = []
+let snake = [];
 snake[0] = {
     x: 9 * box,
     y: 10 * box
-}
+};
 
 // create the food
 let food = {
     x: Math.floor(Math.random()*17+1) * box,
     y: Math.floor(Math.random()*15*3) * box
-}
+};
 
 // create the score var
-let score = 0
+let score = 0;
+
+let d;
 
 // control the snake
 document.addEventListener("keydown", direction);
 
 function direction(event){
     if (event.keyCode == 37){
-        d = "LEFT"
+        d = "LEFT";
     } else if (event.keyCode == 38){
-        d = "UP"
+        d = "UP";
     } else if (event.keyCode == 39){
-        d = "RIGHT"
+        d = "RIGHT";
     } else if (event.keyCode == 40){
-        d = "DOWN"
-    }
+        d = "DOWN";
+    };
 }
 
 //draw everything on canvas
 function draw (){
-    ctx.drawImage(ground,0,0)
 
-    for (let i = 0; i<snake.length; i++){
-        ctx.fillStyle = (i == 0) ? "green":"white"
-        ctx.fillRect(snake[i].x, snake[i].y, box, box)
-    }
+    ctx.drawImage(ground,0,0);
 
-    ctx.drawImage(foodImg, food.x, food.y)
+    for (let i = 0; i < snake.length; i++){
+        ctx.fillStyle = (i == 0) ? "green":"white";
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
 
-    ctx.fillStyle = "white"
-    ctx.font = "45px Changa one"
-    ctx.fillText(score, 2*box, 1.6*box)
+        ctx.strokeStyle = "red";
+        ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+    };
+
+    ctx.drawImage(foodImg, food.x, food.y);
+
+    // old head position
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (d == "LEFT") snakeX -= box;
+    if (d == "UP") snakeY -= box;
+    if (d == "RIGHT") snakeX += box;
+    if (d == "DOWN") snakeY += box;
+    // remove the tail
+    snake.pop();
+    
+    // add new head
+    let newHead ={
+        x: snakeX,
+        y: snakeY
+    };
+
+    snake.unshift(newHead);
+
+    ctx.fillStyle = "white";
+    ctx.font = "45px Changa one";
+    ctx.fillText(score, 2*box, 1.6*box);
 }
 
-let game = setInterval(draw,100)
+let game = setInterval(draw,100);
 
 
