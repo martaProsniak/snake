52f8384a305ccd5aee97600c42a9cb57d4ea762d
martaprosniak <martaprosniak@gmail.com>
2019-07-06 23:59:04 +0200

    variables added

---
+++ snake.js
@@ -0,0 +1,30 @@
+const cvs = document.getElementById("snake")
+const ctx = cvs.getContext("2d");
+
+// create the unit
+const box = 32
+
+// load images
+const ground = new Image()
+ground.src = "img/ground.png"
+
+const foodImg = new Image();
+foodImg.src = "img/food.png"
+
+// create the snake
+let snake = []
+snake[0] = {
+    x: 9 * box,
+    y: 10 * box
+}
+
+// create the food
+let food = {
+    x: Math.floor(Math.random()*17+1) * box,
+    y: Math.floor(Math.random()*15*3) * box
+}
+
+// create the score var
+let score = 0
+
+
